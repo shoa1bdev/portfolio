3c000831026107ba885ec9d63ad2dc846a391b1f
Shoaib <s.akhtar7958@gmail.com>
2025-06-09 00:51:49 +0530

    Big Update #1

--- main.js
+++ main.js
@@ -4,3 +4,15 @@ const hamburger = document.getElementById('hamburger');
   hamburger.addEventListener('click', () => {
     navlinks.classList.toggle('active');
   });
+
+const backgroundShape = document.getElementById('second')
+const aboutMe = document.getElementById('about-me')
+
+// function matchHeight() {
+//   const olHeight = aboutMe.offsetHeight * 1.3;
+//     backgroundShape.style.height = `${olHeight}px`;
+//     console.log(olHeight)
+//   }
+
+//   window.addEventListener('load', matchHeight);
+//   window.addEventListener('resize', matchHeight);
